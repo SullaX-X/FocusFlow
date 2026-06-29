60c018f2f43420211fb4933510bda64512ed6385
SullaX-X <korsullilak@gmail.com>
2026-06-29 22:24:19 +0300

    FocusFlow

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/FocusFlow/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
